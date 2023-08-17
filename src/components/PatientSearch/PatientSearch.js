cefc17902e3f265dcca8200690b372f048d7df13
Simon Kruik <simonkruik@gmail.com>
2023-08-17 14:52:05 +0300

    Added initial OpenMRS API code

--- src/components/PatientSearch/PatientSearch.js
+++ src/components/PatientSearch/PatientSearch.js
@@ -67,13 +67,31 @@ const rows=[
 ]
 
 const SearchFunction = () => {
+    const requestHeaders = new Headers();
+    console.log("Searching for patients...");
+    requestHeaders.append("Authorization", "Basic " + btoa( process.env.REACT_APP_API_USER + ':' + process.env.REACT_APP_API_PASS));
+
+    const requestOptions = {
+        method: 'GET',
+        headers: requestHeaders,
+        redirect: 'follow',
+        accept: "application/json",
+    };
+    console.log("Using headers: ", requestHeaders);
+
+    const url = "https://" + process.env.REACT_APP_BAHMNI_HOST + "/" + process.env.REACT_APP_OPENMRS_API_PATH + "/patient?q=Test&limit=10";
+    console.log("Using URL: ",url);
+    fetch( url, requestOptions)
+        .then(response => response.text())
+        .then(result => console.log(result))
+        .catch(error => console.log('error',error));
     return null;
 }
 
 const PatientSearch = (props) => {
     return (
     <>
-    <PatientTable headers={headers} rows={rows} />
+    <PatientTable headers={headers} rows={rows} searchAction={SearchFunction} />
     </>
     );
 }
