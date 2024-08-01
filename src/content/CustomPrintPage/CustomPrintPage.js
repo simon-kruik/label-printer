52a3509cfe039d71a97202c15f91909468b55bbd
Simon Kruik <simon.karate.kid@gmail.com>
2024-08-01 12:36:13 +0300

    Got Barcodes included now

--- src/content/CustomPrintPage/CustomPrintPage.js
+++ src/content/CustomPrintPage/CustomPrintPage.js
@@ -1,8 +1,10 @@
 import React from 'react';
 //import ReactPDF from '@react-pdf/renderer';
-import { Page, Text, View, Document, StyleSheet, } from '@react-pdf/renderer';
+import { Page, Text, View, Document, StyleSheet, Image,} from '@react-pdf/renderer';
 import {PDFViewer,  } from '@react-pdf/renderer';
 import PrintButton from '../../components/PrintButton';
+import JsBarcode from 'jsbarcode';
+let canvas;
 
 const styles = StyleSheet.create({
     page: {
@@ -15,27 +17,36 @@ const styles = StyleSheet.create({
     text: {
         textAlign: 'justify',
         overflowWrap: 'break-word',
-        fontSize: '14',
+        fontSize: '13',
         fontWeight: 'bold',
 
+    },
+    image: {
+        height: 28,
+        width: 100,
+   //     transform: "rotate(270deg)"
     }
 })
 
 const CustomPrintPage = () => {
+    canvas = document.createElement('canvas');
+    JsBarcode(canvas, 'MAF224785', {"textPosition":"top","height":30,});
+    const barcode = canvas.toDataURL();
     return (
     <>
   <PDFViewer showToolbar={true} id="LabelPDF"> 
         <Document>
         <Page size={[25, 50]} orientation="landscape" dpi={203} style={styles.page}>
             <View>
+
                 <View style={styles.text}>
-                    <Text>MAF202429<br/></Text>
+                    <Text>Test Brian<br/></Text>
                 </View>
                 <View style={styles.text}>
-                    <Text>Simon Kruik<br/></Text>
+                    <Text>M | 61<br/></Text>
                 </View>
                 <View style={styles.text}>
-                    <Text>M | 25<br/></Text>
+                    <Image src={barcode} style={styles.image}/>
                 </View>
             </View>
 
